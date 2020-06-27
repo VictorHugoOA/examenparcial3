16cf4086b3df738270602ece84a6cc01d27330fa
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2020-06-27 17:36:29 -0500

    Arreglado

--- nodejs/controlador.js
+++ nodejs/controlador.js
@@ -5,7 +5,7 @@ const ctrlDatos =
     findState: async (req, res) =>
     {
         const data = await dataModel.getState(req.params.estado);
-        res.json({clave: data.data.datos[0].cve_agee, nombre: data.data.datos[0].nom_agee, poblacion: data.data.datos[0].pob, femenina: data.data.datos[0].opb_fem, masculina: data.data.datos[0].pob_mas, viviendas: data.data.datos[0].viv});
+        res.json({clave: data.data.datos[0].cve_agee, nombre: data.data.datos[0].nom_agee, poblacion: data.data.datos[0].pob, femenina: data.data.datos[0].pob_fem, masculina: data.data.datos[0].pob_mas, viviendas: data.data.datos[0].viv});
     },
 
     findCity: async (req, res) =>
