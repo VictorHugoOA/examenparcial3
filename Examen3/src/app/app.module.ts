4271e366f578ce01fbfcd8fc1c0f048806769e48
NRekits <64814187+NRekits@users.noreply.github.com>
2020-06-27 22:59:47 -0500

    Final

--- Examen3/src/app/app.module.ts
+++ Examen3/src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraficaComponent } from './components/grafica/grafica.component';
-import { NavComponent } from './components/nav/nav.component';
 import { EstadoComponent } from './components/estado/estado.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './components/services/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraficaComponent,
-    NavComponent,
     EstadoComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [DataService],
--- Examen3/src/app/components/estado/estado.component.ts
+++ Examen3/src/app/components/estado/estado.component.ts
@@ -1,56 +1,93 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService,estado, municipio } from '../services/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { DataService, estado, municipio } from '../services/data.service';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-estado',
   templateUrl: './estado.component.html',
-  styleUrls: ['./estado.component.css']
+  styleUrls: ['./estado.component.css'],
 })
 export class EstadoComponent implements OnInit {
-  est:string;
-  Municipios:municipio[];
-  estado:estado= new estado();
-  constructor(private data:DataService, private router:ActivatedRoute) { 
-  
-    this.router.params.subscribe((params) => {
-     this.est=params["estado"];
-     
-    });
-    let url =`http://localhost:3000/state/${this.est}`;
+
+  est: string;
+  Municipios: municipio[];
+  bandera:boolean;
+  estado: estado = new estado();
+  private chart: any = null;
+  private label: string[] = ['Mujeres', 'Hombres'];
+  private dat: number[] = [];
+
+  constructor(private data: DataService) {
+    this.bandera=true;
+  }
+
+  Buscar(estado:string) {
+    this.est=estado;
+    this.bandera=false;
+    let url = `http://localhost:3000/state/${this.est}`;
     console.log(this.est);
 
-    this.data.consulta(url).subscribe( (Data:any) =>{
-  
-    
-      this.estado.clave= Data.clave;
-      this.estado.nombre=Data.nombre;
-      this.estado.poblacion=Data.poblacion;
-      this.estado.vivienda=Data.viviendas;
-      this.estado.femenina=Data.femenina;
-      this.estado.masculina=Data.masculina;
-      
-      let url2=`http://localhost:3000/cities/${this.estado.clave}`;
+    this.data.consulta(url).subscribe((Data: any) => {
+      this.estado.clave = Data.clave;
+      this.estado.nombre = Data.nombre;
+      this.estado.poblacion = Data.poblacion;
+      this.estado.vivienda = Data.viviendas;
+      this.estado.femenina = Data.femenina;
+      this.estado.masculina = Data.masculina;
+      this.dat[0] = Data.femenina;
+      this.dat[1] = Data.masculina;
 
-      this.data.consulta(url2).subscribe((data2:any[]) =>{
-      
-       data2.forEach(element => {
+      //grafica
+      this.chart = new Chart('my chart', {
+        type: 'bar',
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: 'Población por sexo',
+              data: this.dat,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+              ],
+              borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+      let url2 = `http://localhost:3000/cities/${this.estado.clave}`;
+
+      this.data.consulta(url2).subscribe((data2: any[]) => {
+        this.Municipios = [];
+        data2.forEach((element) => {
           let m = new municipio();
-          m.claveEst=element.clave_e;
+          m.claveEst = element.clave_e;
           m.claveMun = element.clave;
           m.nombre = element.nombre;
-          m.poblacion= element.poblacion;
-          m.vivienda=element.viviendas;
-          m.masculina=element.masculina;
-          m.femenina=element.femenina;
+          m.poblacion = element.poblacion;
+          m.vivienda = element.viviendas;
+          m.masculina = element.masculina;
+          m.femenina = element.femenina;
           this.Municipios.push(m);
         });
       });
     });
-   
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
